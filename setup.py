2faf606a3e68b36951a0809cf37081c1f08912df
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-12-11 21:33:16 +0100

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ This projects focus is heavily on simplicity and speed.
 # Setting up
 setup(
     name="pyfys",
-    version="0.2.12",
+    version="0.2.13",
     author="Nikolai G. Borbe",
     url="https://github.com/nikolaiborbe",
     author_email="nikolaiborbe@gmail.com",
